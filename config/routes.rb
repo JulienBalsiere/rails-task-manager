5cf35f74fffaab9f6c30e4268353ac5be84523ab
Julien Balsiere <julien.balsiere@gmail.com>
2025-11-25 14:14:49 +0100

    Rails Task Manager app working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,22 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # # INDEX
+  # get "tasks", to: "tasks#index"
+  # # NEW
+  # get "tasks/new", to: "tasks#new"
+  # # EDIT
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # # SHOW
+  # get "tasks/:id", to: "tasks#show", as: 'task'
+  # # UPDATE
+  # patch "tasks/:id", to: "tasks#update"
+  # # CREATE
+  # post "tasks", to: "tasks#create"
+  # # DESTROY
+  # delete "tasks/:id", to: "tasks#destroy"
+
+  resources :tasks
   # Defines the root path route ("/")
   # root "posts#index"
 end
